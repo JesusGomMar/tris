b87b8b96fb381973ea7aa3a0a61ed03888046466
JesusGM <jgmartin88@gmail.com>
2023-02-04 11:12:13 +0100

    Ficheros sql

--- src/main/resources/Sql/tareas_usuario.sql
+++ src/main/resources/Sql/tareas_usuario.sql
@@ -1,15 +1,11 @@
-CREATE TABLE `tris`.`tareas_usuario` (
-  `id_usuario` INT NOT NULL,
-  `id_tarea` INT NOT NULL,
-  INDEX `fk_usuario_tarea_idx` (`id_usuario` ASC) VISIBLE,
-  INDEX `fk_tarea_usuario_tarea_idx` (`id_tarea` ASC) VISIBLE,
-  CONSTRAINT `fk_usuario_usuario_tarea`
-    FOREIGN KEY (`id_usuario`)
-    REFERENCES `tris`.`usuarios` (`idUsuarios`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tarea_usuario_tarea`
-    FOREIGN KEY (`id_tarea`)
-    REFERENCES `tris`.`tareas` (`id_tarea`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+CREATE TABLE `tareas_usuario` (
+  `id_usuario` int NOT NULL,
+  `id_tarea` int NOT NULL,
+  `tiempo` varchar(45) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `comentarios` varchar(450) DEFAULT NULL,
+  KEY `fk_usuario_tarea_idx` (`id_usuario`),
+  KEY `fk_tarea_usuario_tarea_idx` (`id_tarea`),
+  CONSTRAINT `fk_tarea_usuario_tarea` FOREIGN KEY (`id_tarea`) REFERENCES `tareas` (`id_tarea`),
+  CONSTRAINT `fk_usuario_usuario_tarea` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`idUsuarios`)
+)
